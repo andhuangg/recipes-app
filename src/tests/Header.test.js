6e43c7effb727d1a89d3847ed40b5c0ec9f6e509
tix <laissa.soares@geraestec.com.br>
2023-05-10 17:53:43 -0300

    Sessão header 07 a 09 completos

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,59 +1,65 @@
 import React from 'react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import PropTypes from 'prop-types';
+import { getByTestId, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Header from '../components/Header';
+import { renderWithRouter } from './helpers/renderWith';
+import App from '../App';
+import AppProvider from '../context/AppProvider';
+import RecipeProvider from '../context/RecipeProvider';
+import HeaderProvider from '../context/HeaderProvider';
 
-test('Redenrizando o header', () => {
-  render(<Header />);
-});
+describe('testes pagina de header', () => {
+  test('Redenrizando o header', () => {
+    renderWithRouter(<Header />);
+  });
 
-test('Testanto testids', () => {
-  const { getByTestId } = render(<Header />);
-  expect(getByTestId('profile-top-btn')).toBeInTheDocument();
-  expect(getByTestId('search-top-btn')).toBeInTheDocument();
-  // expect(getByTestId('search-input')).toBeInTheDocument();
-  expect(getByTestId('page-title')).toBeInTheDocument();
-});
+  test('Testanto testids', () => {
+    renderWithRouter(<Header />);
+    expect(getByTestId('profile-top-btn')).toBeInTheDocument();
+    // expect(getByTestId('search-top-btn')).toBeInTheDocument();
+    expect(getByTestId('page-title')).toBeInTheDocument();
+  });
 
-test('Testanto history', () => {
-  const history = createMemoryHistory();
-  const { getByTestId } = render(
-    <Router history={ history }>
-      <Header />
-    </Router>,
-  );
-  const button = getByTestId('profile-top-btn');
-  fireEvent.click(button);
-  expect(history.location.pathname).toBe('/profile');
-});
+  test('Testanto history', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <Header />
+      </Router>,
+    );
+    const button = getByTestId('profile-top-btn');
+    userEvent.click(button);
+    expect(history.location.pathname).toBe('/profile');
+  });
 
-test('Testanto evento', () => {
-  const { getByTestId } = render(<Header />);
-  const input = getByTestId('search-input');
-  fireEvent.click(input);
-});
+  test('testando função handleClick', async () => {
+    renderWithRouter(
+      <HeaderProvider>
+        <RecipeProvider>
+          <AppProvider>
+            <App />
+          </AppProvider>
+        </RecipeProvider>
+      </HeaderProvider>,
+    );
+    // history.push('/meals');
+    const testEmail = screen.getByTestId('email-input');
+    const testSenha = screen.getByTestId('password-input');
+    const buttonUser = screen.getByRole('button');
 
-test('testando função handleClick', () => {
-  render(<Header />);
-  const searchButton = getByTestId('search-top-btn');
-  const searchInput = getByTestId('search-input');
-  expect(searchInput).not.toBeInTheDocument();
+    userEvent.type(testEmail, 'laissa@laissa.com');
+    userEvent.type(testSenha, 'laissalaissa.com');
+    userEvent.click(buttonUser);
 
-  fireEvent.click(searchButton);
+    const searchButton = await screen.findByTestId('button-search');
+    userEvent.click(searchButton);
 
-  expect(searchInput).toBeInTheDocument();
+    const searchInput = await screen.findByTestId('search-input');
+    expect(searchInput).toBeInTheDocument();
 
-  fireEvent.click(searchButton);
-
-  expect(searchInput).not.toBeInTheDocument();
-});
-
-test('Testanto proptypes', () => {
-  expect(Header).toEqual({
-    history: PropTypes.shape({
-      push: PropTypes.func,
-    }).isRequired,
+    userEvent.click(searchButton);
+    expect(searchInput).not.toBeInTheDocument();
   });
 });
